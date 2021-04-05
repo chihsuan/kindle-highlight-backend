c7734a6cddc56cef42a373fd5f9269ff2ee03c59
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2021-04-05 16:46:56 +0800

    Remove comment fmt

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -56,7 +56,6 @@ func AuthMiddleware(auto401 bool) gin.HandlerFunc {
 		}
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			my_user_id := uint(claims["id"].(float64))
-			//fmt.Println(my_user_id,claims["id"])
 			UpdateContextUserModel(c, my_user_id)
 		}
 	}
